be5a6c7e3fffa3931e05184099e4daa5625f1ae0
Vitoria-m <vitoriazzz@outlook.com>
2022-05-05 17:53:44 -0300

    consumindo API

--- src/script.js
+++ src/script.js
@@ -1,7 +1,27 @@
 fetch(`http://localhost:5000/products`)
-.then(res => {
-    return res.json()
-})
-.then(data => {
-   console.log(data)
-})
+    .then(res => {
+        return res.json()  
+    })
+    .then(data => {
+        let data1 = '';
+        data.map((item) => {
+            data1+= `
+                <h1>${item.name}</h1>
+                <span id="span">${item.price}</span>
+            `
+        })
+
+        document.getElementById('main').innerHTML = data1
+
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+    
+
+
+
+
+
+
