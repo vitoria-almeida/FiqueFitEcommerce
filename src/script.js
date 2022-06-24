6e3566fdaa7dcf38794b003122d05c6c99dd062c
Vitoria-m <vitoriazzz@outlook.com>
2022-06-24 16:46:24 -0300

    alteracoes no checkbox

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
-fetch(`http://localhost:5000/products`)
+function getAPI() {
+    let url = 'http://localhost:5000/products'
+    return fetch(url)
     .then(res => {
         return res.json()  
     })
@@ -6,7 +8,7 @@ fetch(`http://localhost:5000/products`)
         let getData = '';
         data.map((item) => {
             getData+= `
-                    <div id="cardProduct">
+                    <div class="cardProduct">
                     <div id="productCard">
                         <div><img src="${item.image}"/></div>
                         <h3>${item.name.toUpperCase()}</h3>
@@ -16,13 +18,18 @@ fetch(`http://localhost:5000/products`)
                     </div>   
                     </div>           
                 `
-        })
-
+        })   
         document.getElementById('centerContent').innerHTML = getData
+        return data
+
     })
     .catch(err => {
         console.log(err)
     })
+
+}
+
+getAPI()
 
 
 //filter accordion
@@ -40,7 +47,7 @@ let loadMore = document.querySelector('#loadButton')
 let currentItem = 8;
 
 loadMore.onclick = () => {
-    let boxes = [...document.querySelectorAll('#cardProduct')]
+    let boxes = [...document.querySelectorAll('.cardProduct')]
     for (var i = currentItem; i < currentItem+4; i++) {
         boxes[i].style.display = 'inline-block'
     }
@@ -51,6 +58,62 @@ loadMore.onclick = () => {
         document.getElementById('footer').style.margin = '50px 0px 0px'
     }  
 }
+
+
+//checkbox
+let sizeP = document.getElementById('p')
+// let boxes = document.getElementsByClassName('cardProduct')
+
+function size() {
+    getAPI().then(data => {
+        
+        let dataAPI = data
+        
+
+            sizeP.addEventListener('change', () => {
+                const isChecked = sizeP.checked
+
+                dataAPI.map((item) => {
+                    if(isChecked) {  
+                        if(item.size !== "M")   {
+                            console.log(`${item.size}`)                   
+                             // [...document.getElementsByClassName('cardProduct')].forEach(e => e.style.display = "none")    
+                        }                        
+                                                                  
+                    }
+                    else {
+                        console.log(`xau`)
+                        // boxes.style.display = 'inline-block'
+                    }
+                })
+        })     
+    })
+}
+
+size()
+
+
+
+
+
+
+
+                       
+    
+
+ 
+//         if(`${item.size} !== "P"`) {
+//             boxes.style.display = 'none'
+
+// check.addEventListener('change', () => {
+//     alert(`${item.size}`)
+//     alert('oii')
+//     // let boxes = [...document.querySelectorAll('#cardProduct')]
+    
+//     // if(`${item.size} === "P"`) {
+//     //     boxes[i].style.display = 'none'
+//     // }
+// })
 
 
 
